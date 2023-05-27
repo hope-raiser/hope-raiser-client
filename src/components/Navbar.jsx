68fa4aad2a1d4055ebd8b41ae7496b3fa067edea
LG <idreesahmadx45@gmail.com>
2023-05-27 12:28:45 +0700

    create responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import useAuthStore from "@/modules/authStore";
 import { useRouter } from "next/router";
 import Link from "next/link";
-import { Image, Img } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 import { useEffect, useState } from "react";
 
@@ -10,6 +11,7 @@ const Navbar = () => {
   const router = useRouter();
   const [isLogin, setIsLogin] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const [navVisible, setNavVisible] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -25,79 +27,130 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleNavbar = () => {
+    setNavVisible(!navVisible);
+  };
+
   return (
     <>
-      <nav className="bg-[#D9D9D9]">
-        <div className="py-5">
-          <div className="flex items-center  justify-around">
-            <div className="flex items-center ">
+      <nav className="bg-[#D9D9D9] ">
+        <div className="flex flex-wrap p-4 md:px-4  items-center gap-4 justify-around">
+          <Link href="/">
+            <div className="flex items-center">
               <Image src="https://placehold.co/60" fallback="https://placehold.co/75" alt="LOGO" className="text-Dark font-semibold" />
               <div className="flex flex-wrap flex-col px-4 text-center">
                 <h2 className="text-Teal text-2xl font-bold tracking-wider">HOPERAISER</h2>
-                <span className="text-Dark text-xs  tracking-widest ">Empowering Hope, Fueling Progress</span>
+                <span className="text-Dark text-xs  tracking-widest hidden md:block">Empowering Hope, Fueling Progress</span>
               </div>
             </div>
-            <div className="text-Dark text-lg font-medium flex gap-10">
-              <Link href="/" className="hover:text-Teal duration-300">
-                Homepage
+          </Link>
+          {/* NAVBAR MD > */}
+          <ul className="text-Dark text-lg font-medium hidden md:flex gap-10 ">
+            <Link href="/categories" className="hover:text-Teal duration-300">
+              Categories
+            </Link>
+            <Link href={isLogin ? "/users/bookmark" : "/login"} className="hover:text-Teal duration-300">
+              Saved
+            </Link>
+          </ul>
+          <div className="hidden md:flex">
+            {!isLogin ? (
+              <>
+                <Link href="/login">
+                  <button className="bg-Teal px-5 py-2 text-sm text-slate-200 rounded-sm hover:text-slate-300 hover:bg-teal-700 duration-500">
+                    Login
+                  </button>
+                </Link>
+              </>
+            ) : (
+              <>
+                <div className="relative">
+                  <button
+                    onClick={toggleDropdown}
+                    className="bg-Teal px-5 py-2 text-sm text-slate-200 rounded-sm hover:text-slate-300 hover:bg-teal-700 duration-500"
+                  >
+                    Menu
+                  </button>
+                  {isOpen && (
+                    <div className="absolute right-0 z-10 mt-2  bg-slate-50 rounded-sm shadow-md ">
+                      <ul className="py-2 px-8 ">
+                        <Link href="/users">
+                          <li className="cursor-pointer mb-4 py-1  text-Dark hover:text-Teal duration-300">Profile</li>
+                        </Link>
+                        <Link href="/campaigns/create">
+                          <li className="cursor-pointer mb-4 py-1  text-Dark hover:text-Teal duration-300">Create Campaign</li>
+                        </Link>
+                        <li
+                          className=" cursor-pointer  text-Dark hover:text-Teal py-1"
+                          onClick={() => {
+                            window.localStorage.removeItem("token");
+                            window.localStorage.removeItem("user");
+                            setIsLogin(false);
+                            router.push("/");
+                          }}
+                        >
+                          Logout
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </>
+            )}
+          </div>
+
+          {/* NAVBAR SM << */}
+          <div className="block md:hidden " onClick={handleNavbar}>
+            <AiOutlineMenu size={20} />
+          </div>
+          <div
+            className={
+              navVisible
+                ? "z-[30] md:hidden fixed h-full  top-0 left-0 p-8 w-full bg-Dark duration-500 ease-in-out"
+                : "z-[30] md:hidden fixed h-full top-0 left-[-1000px] p-8 w-full bg-Dark duration-500 ease-in-out"
+            }
+          >
+            <div className="flex flex-wrap overflow-auto items-center justify-around">
+              <Link href="/">
+                <div className="flex items-center ">
+                  <Image src="https://placehold.co/60" fallback="https://placehold.co/60" alt="LOGO" className="text-Dark font-semibold" />
+                  <div className="flex flex-wrap flex-col px-4 text-center">
+                    <h2 className="text-Teal text-2xl font-bold tracking-wider">HOPERAISER</h2>
+                  </div>
+                </div>
               </Link>
-              <Link href="/categories" className="hover:text-Teal duration-300">
+              <div className="block md:hidden " onClick={handleNavbar}>
+                <AiOutlineClose size={20} color="white" />
+              </div>
+            </div>
+            <ul className="text-white text-md  font-medium pt-8" onClick={handleNavbar}>
+              <Link href="/categories" className="block py-4 border-b border-gray-700">
                 Categories
               </Link>
-              <Link href={isLogin ? "/users/bookmark" : "/login"} className="hover:text-Teal duration-300">
+              <Link href={isLogin ? "/users/bookmark" : "/login"} className="block py-4 border-b border-gray-700">
                 Saved
               </Link>
-            </div>
-            <div>
-              {!isLogin ? (
-                <>
-                  <Link href="/login">
-                    <button className="bg-Teal px-5 py-2 text-sm text-slate-200 rounded-sm hover:bg-slate-100 hover:text-Teal duration-500 hover:ring-1 hover:ring-Teal">
-                      Login
-                    </button>
-                  </Link>
-                </>
-              ) : (
-                <>
-                  <div className="relative">
-                    <button
-                      onClick={toggleDropdown}
-                      className="bg-Teal px-5 py-2 text-sm text-slate-200 rounded-sm hover:bg-slate-100 hover:text-Teal duration-500 hover:ring-1 hover:ring-Teal"
-                    >
-                      Menu
-                    </button>
-                    {isOpen && (
-                      <div className="absolute z-10 mt-2  bg-slate-50 rounded-sm shadow-md ">
-                        <ul className="py-2 px-8 ">
-                          <li
-                            className="cursor-pointer mb-4 py-1  text-Dark hover:text-Teal duration-300"
-                            onClick={() => router.push("/campaigns/create")}
-                          >
-                            Create Campaign
-                          </li>
-                          <li
-                            className="cursor-pointer mb-4 py-1  text-Dark hover:text-Teal duration-300"
-                            onClick={() => router.push("/profile")}
-                          >
-                            Profile
-                          </li>
-                          <li
-                            className=" cursor-pointer  text-Dark hover:text-Teal py-1"
-                            onClick={() => {
-                              window.localStorage.removeItem("token");
-                              setIsLogin(false);
-                              router.push("/");
-                            }}
-                          >
-                            Logout
-                          </li>
-                        </ul>
-                      </div>
-                    )}
-                  </div>
-                </>
-              )}
-            </div>
+              <Link href={isLogin ? "users" : "/login"} className="block py-4 mt-8  border-b border-gray-700">
+                Profile
+              </Link>
+              <Link href={isLogin ? "campaigns/create" : "/login"} className="block py-4 border-b border-gray-700">
+                Create Campaign
+              </Link>
+              <div className="flex justify-center">
+                <button
+                  className="bg-Teal px-5 py-2 mt-6  text-sm text-slate-200 rounded-sm "
+                  onClick={() => {
+                    window.localStorage.removeItem("token");
+                    window.localStorage.removeItem("user");
+                    setIsLogin(false);
+                    window.location.reload();
+                    router.push("/");
+                  }}
+                >
+                  Logout
+                </button>
+              </div>
+            </ul>
           </div>
         </div>
       </nav>
--- src/pages/categories/index.page.js
+++ src/pages/categories/index.page.js
@@ -1,133 +1,116 @@
 import Layout from "@/components/Layout";
 import { useEffect, useState } from "react";
-import {
-	CircularProgress,
-	SimpleGrid,
-	Box,
-	Heading,
-	Text,
-	Center,
-	Flex,
-} from "@chakra-ui/react";
+import { CircularProgress, SimpleGrid, Box, Heading, Text, Center, Flex, Container } from "@chakra-ui/react";
 import { getAllCategory } from "@/modules/fetch/categories";
 import { useRouter } from "next/router";
 
 function Category() {
-	const [categories, setCategories] = useState("");
-	const [isLoading, setLoading] = useState(true);
-	const router = useRouter();
+  const [categories, setCategories] = useState("");
+  const [isLoading, setLoading] = useState(true);
+  const router = useRouter();
 
-	useEffect(() => {
-		Promise.all([getAllCategory()]).then((values) => {
-			setCategories(...values);
-			setLoading(false);
-		});
-	}, []);
+  useEffect(() => {
+    Promise.all([getAllCategory()]).then((values) => {
+      setCategories(...values);
+      setLoading(false);
+    });
+  }, []);
 
-	if (isLoading) {
-		return (
-			<>
-				<Flex height="full" width="full" align="center">
-					<CircularProgress isIndeterminate color="green.300" />
-				</Flex>
-			</>
-		);
-	}
+  if (isLoading) {
+    return (
+      <>
+        <Flex height="full" width="full" align="center">
+          <CircularProgress isIndeterminate color="green.300" />
+        </Flex>
+      </>
+    );
+  }
 
-	function getColor(index) {
-		switch (index) {
-			case 0:
-				return "#395B98";
-			case 1:
-				return "#F11759";
-			case 2:
-				return "#00C1CF";
-			case 3:
-				return "#FFBB55";
-			case 4:
-				return "#D9D9D9";
-			case 5:
-				return "#1DAA97";
-			case 6:
-				return "#E683FF";
-			case 7:
-				return "#975A16";
-			default:
-				return "gray.200";
-		}
-	}
+  function getColor(index) {
+    switch (index) {
+      case 0:
+        return "#395B98";
+      case 1:
+        return "#F11759";
+      case 2:
+        return "#00C1CF";
+      case 3:
+        return "#FFBB55";
+      case 4:
+        return "#D9D9D9";
+      case 5:
+        return "#1DAA97";
+      case 6:
+        return "#E683FF";
+      case 7:
+        return "#975A16";
+      default:
+        return "gray.200";
+    }
+  }
 
-	const data = categories.map((category, index) => ({
-		...category,
-		color: getColor(index),
-	}));
+  const data = categories.map((category, index) => ({
+    ...category,
+    color: getColor(index)
+  }));
 
-	return (
-		<>
-			<Layout>
-				<Heading
-					textAlign="center"
-					pt={{ base: 8, sm: 20 }}
-					fontSize={{ base: "4xl", sm: "6xl" }}
-				>
-					Explore Causes
-				</Heading>
-				<Center>
-					<SimpleGrid
-						columns={{ base: 1, sm: 2, lg: 3 }}
-						spacing={{ base: 4, md: 8 }}
-						py={{ base: 8, md: 20 }}
-						px={4}
-					>
-						{data.map((category) => {
-							return (
-								<>
-									<Box
-										bg={category.color}
-										h={{ sm: 40, md: 64, lg: 80 }}
-										w={{ sm: 40, md: 64, lg: 80 }}
-										rounded={"md"}
-										display={"flex"}
-										alignItems={"flex-end"}
-										onClick={() => {
-											router.push({
-												pathname: "/",
-												query: {
-													category_id: category.id,
-												},
-											});
-										}}
-										transition="ease .4s"
-										_hover={{
-											transform: "scale(1.08)",
-											color: "blackAlpha.800",
-										}}
-										cursor={"pointer"}
-									>
-										<Text
-											w="full"
-											fontWeight={{
-												base: "semibold",
-												sm: "bold",
-											}}
-											py={2}
-											px={4}
-											fontSize={{
-												base: "2xl",
-												sm: "3xl",
-											}}
-										>
-											{category.name}
-										</Text>
-									</Box>
-								</>
-							);
-						})}
-					</SimpleGrid>
-				</Center>
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Layout>
+        <Heading textAlign="center" pt={{ base: 8, sm: 20 }} fontSize={{ base: "3xl", sm: "6xl" }}>
+          Explore Causes
+        </Heading>
+        <Center>
+          <SimpleGrid columns={{ base: 2, lg: 3 }} spacing={{ base: 4, xl: 8 }} py={{ base: 8, md: 20 }} px={4}>
+            {data.map((category) => {
+              return (
+                <>
+                  <Box
+                    bg={category.color}
+                    h={{ base: 60, xl: 80 }}
+                    w={{ base: "auto", xl: 80 }}
+                    rounded={"md"}
+                    display={"flex"}
+                    alignItems={"flex-end"}
+                    onClick={() => {
+                      router.push({
+                        pathname: "/",
+                        query: {
+                          category_id: category.id
+                        }
+                      });
+                    }}
+                    transition="ease .4s"
+                    _hover={{
+                      transform: "scale(1.08)",
+                      color: "blackAlpha.800"
+                    }}
+                    cursor={"pointer"}
+                  >
+                    <Text
+                      w="full"
+                      fontWeight={{
+                        base: "semibold",
+                        sm: "bold"
+                      }}
+                      py={2}
+                      px={4}
+                      fontSize={{
+                        base: "xl",
+                        xl: "3xl"
+                      }}
+                    >
+                      {category.name}
+                    </Text>
+                  </Box>
+                </>
+              );
+            })}
+          </SimpleGrid>
+        </Center>
+      </Layout>
+    </>
+  );
 }
 
 export default Category;
